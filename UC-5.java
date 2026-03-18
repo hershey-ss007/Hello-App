4afc910a452130caa7a65310768adbdb46e3309a
hershey-ss007 <hm4585@srmist.edu.in>
2026-03-18 08:46:43 +0530

    coded UC-5

---
+++ UC-5.java
@@ -0,0 +1,20 @@
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        StringBuilder names = new StringBuilder();
+
+        if (args.length == 0) {
+            names.append("World");
+        } else {
+            for (String name : args) {
+                if (names.length() > 0) {
+                    names.append(", ");
+                }
+                names.append(name);
+            }
+        }
+
+        System.out.println("Hello, " + names + "!");
+    }
+}
